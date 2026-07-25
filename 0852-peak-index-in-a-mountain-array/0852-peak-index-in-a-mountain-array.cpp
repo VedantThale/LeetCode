c4520cf15a4cf428cc7ab54f0a8cc743124d33ed
Vedant Thale <vedant.thale21@gmail.com>
2026-07-25 11:16:46 +0530

    Time: 0 ms (100%), Space: 63.4 MB (98.46%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int start=0 ,end=arr.size()-1,mid;
+        int start =0,end=arr.size()-1,mid;
         while(start<=end)
         {
-            mid=end+(start-end)/2;
-
-            //Peak Value
-            if (arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-            return mid;
-
-            // Move Right Side
-            else if(arr[mid]>arr[mid-1])
-            start=mid+1;
-            
-            // Move Left Side
+            mid=start+(end-start)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid+1]<arr[mid])
+            return mid ;
+            else if(arr[mid+1]>arr[mid])
+            start++;
             else
-            end=mid-1;
+            end--;
         }
-        return -1;
-        
+        return mid;
     }
 };
